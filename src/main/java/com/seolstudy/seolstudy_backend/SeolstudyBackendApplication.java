c274f714e81ee8dc07a4ad6b93190c514c10388a
sowogy <dnjsrb8013@naver.com>
2026-02-02 09:32:08 +0900

    환경 벼수 설정

---
+++ src/main/java/com/seolstudy/seolstudy_backend/SeolstudyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seolstudy.seolstudy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeolstudyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeolstudyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seolstudy/seolstudy_backend/SeolstudyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seolstudy.seolstudy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeolstudyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

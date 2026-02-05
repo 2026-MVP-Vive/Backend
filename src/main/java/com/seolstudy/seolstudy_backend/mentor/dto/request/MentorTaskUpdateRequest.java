e06d92b116c319e6454f2424861020b80fba0674
yenalee <lyemee@naver.com>
2026-02-05 19:34:16 +0900

    할일 수정 구현 완료

--- src/main/java/com/seolstudy/seolstudy_backend/mentor/dto/request/MentorTaskUpdateRequest.java
+++ src/main/java/com/seolstudy/seolstudy_backend/mentor/dto/request/MentorTaskUpdateRequest.java
@@ -1,4 +1,13 @@
 package com.seolstudy.seolstudy_backend.mentor.dto.request;
 
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
 public class MentorTaskUpdateRequest {
+
+    private String title;      // nullable
+    private LocalDate date;    // nullable
+    private Long goalId;       // nullable
 }

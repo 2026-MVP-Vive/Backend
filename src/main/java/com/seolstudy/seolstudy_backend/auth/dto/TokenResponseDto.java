59617134589f5f319e7c93e52b76073a47cd58dc
sowogy <90501828+sowogy@users.noreply.github.com>
2026-02-07 00:44:46 +0900

    Update TokenResponseDto.java

--- src/main/java/com/seolstudy/seolstudy_backend/auth/dto/TokenResponseDto.java
+++ src/main/java/com/seolstudy/seolstudy_backend/auth/dto/TokenResponseDto.java
@@ -1,14 +1,21 @@
 package com.seolstudy.seolstudy_backend.auth.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TokenResponseDto {
     private String accessToken;
+    private String refreshToken;
     private String tokenType;
+    private long expiresIn;
+    @JsonProperty("user")
+    private MemberDto memberDto;
 }
